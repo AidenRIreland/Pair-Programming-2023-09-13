bd33765786a1c621aad188330cb3303c0dae487a
AidenRIreland <aidenireland2004@gmail.com>
2023-09-13 18:29:27 -0400

    Finish code, ran on letcode

--- Main.js
+++ Main.js
@@ -1,32 +1,33 @@
 
  //@param {number[100,4,20,1,3,2]} nums
  //@return {number}
- const nums = [100,4,20,1,5,2,0]
+ //const nums = [100,4,200,1,3,2]
+ //const nums =[1,2,0,1]
 
-var longestConsecutive = function(nums) {
+ var longestConsecutive = function(nums) {
     //1.Declare the values/data types
-    var count = 0;
+    var count = 1;
     var countarray = []
     nums.sort(function(a,b){return a-b})
-    //console.log(nums)
+    console.log(nums)
     //2. find the minuim number in the array
+    if(nums.length === 0){
+        return 0
+    }
     for(let i = 0;i<nums.length; i++){
         if (nums[i+1] === nums[i]+1){
             count++
         }
+        else if(nums[i+1] === nums[i]){
+            continue
+        }
         else{
             countarray.push(count)
-            count = 0
+            count = 1
         }
         
     }
-    console.log(countarray)
     countarray.sort(function(a,b){return a-b})
     return countarray[countarray.length-1]    
-    console.log()
-    //3. Check in the array if the minium in area is plus one
-
-
-    //4.
+    
 };
-console.log(longestConsecutive(nums));
